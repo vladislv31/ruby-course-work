5727f4fa25b9b7e3a741d227147e13253f159177
vladislv31 <vladislv.k31@gmail.com>
2023-05-16 20:32:37 +0300

    resize image

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,13 @@
+require 'mini_magick'
+
 class Restaurant < ApplicationRecord
   has_many :products
   has_one_attached :image
+
+  after_save :crop_image
+
+  def crop_image
+    # return unless image.attached?
+    # image.variant(resize_to_fill: [500, 350]).processed
+  end
 end
